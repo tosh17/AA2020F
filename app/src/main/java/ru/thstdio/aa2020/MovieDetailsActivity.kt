1d34124cc8cdbae123987882792f0eb3922af910
tosh17 <tosh17@list.ru>
2020-11-11 12:13:59 +0400

    rename activity

--- app/src/main/java/ru/thstdio/aa2020/MovieDetailsActivity.kt
+++ app/src/main/java/ru/thstdio/aa2020/MovieDetailsActivity.kt
@@ -3,9 +3,9 @@ package ru.thstdio.aa2020
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_details)
     }
 }
